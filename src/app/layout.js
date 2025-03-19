9b86b12689cd40863a4801faf2d355a04fbb9db0
Henrique Luza <henriqueluza@gmail.com>
2025-03-19 19:07:10 -0300

    Adiciona conteúdo da página layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./globals.css";
+import { Inter } from "next/font/google"; //importa a fonte Inter do google fonts
+import "@fortawesome/fontawesome-free/css/all.min.css"; //importa ícones do font awesome que instalei com npm
+
+const inter = Inter({ subsets: ["latin"] }); //define a fonte Inter como a fonte padrão do projeto
+
+export const metadata = {
+  title: "Termo - Jogo de Palavras",
+  description:
+    "Jogo de adivinhar palavras similar ao Wordle criado com Next.js e Bootstrap",
+  keywords: "jogo, palavras, adivinhar, wordle, nextjs, bootstrap",
+};
 
 export default function RootLayout({ children }) {
+  //função deve se chamar RootLayout e receber um objeto children que representa o conteúdo que será renderizado dentro do layout. Permite ter uma estrutura consistente (cabeçalho, rodapé, menu de navegação) em todas as páginas, sem precisar repetir esse código em cada página.
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
